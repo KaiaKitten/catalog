a02bc0a52b3e837a9c51301590cdcba02482e080
Kyahco <kyahco@gmail.com>
2015-10-01 16:20:12 -0700

    Add 'Add Author' page

--- project.py
+++ project.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, flash
 from sqlalchemy import create_engine, asc
 from sqlalchemy.orm import sessionmaker
 from database_setup import Base, Author, Book, User
@@ -12,10 +12,21 @@ DBSession = sessionmaker(bind=engine)
 session = DBSession()
 
 @app.route('/')
-@app.route('/authors/')
+@app.route('/author/')
 def showAuthors():
     authors = session.query(Author).order_by(asc(Author.name))
     return render_template('authors.html', authors = authors)
+
+@app.route('/author/new/', methods=['GET', 'POST'])    
+def newAuthor():
+    if request.method == 'POST':
+        newAuthor = Author(name=request.form['name'])
+        session.add(newAuthor)
+        flash('New Author %s Successfully Added' % newAuthor.name)
+        session.commit()
+        return redirect(url_for('showAuthors'))
+    else:
+        return render_template('newAuthor.html')
     
 if __name__ == '__main__':
     app.secret_key = 'super_secret_key'
